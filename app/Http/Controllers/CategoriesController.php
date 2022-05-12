483862032befd5724e7a5342d620580d6ea67f31
WisnuRizki <akunpopo2@gmail.com>
2022-05-12 14:22:49 +0700

    Upload Project to Github

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = auth()->user()->categories;
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+ 
+        $cat = new Categories();
+        $cat->name = $request->name;
+ 
+        if (auth()->user()->categories()->save($cat))
+            return response()->json([
+                'success' => true,
+                'data' => $cat->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not added'
+            ], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cat = auth()->user()->categories()->find($id);
+ 
+        if (!$cat) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found '
+            ], 400);
+        }
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $cat->toArray()
+        ], 400);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cat = auth()->user()->categories()->find($id);
+ 
+        if (!$cat) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 400);
+        }
+ 
+        if ($cat->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post can not be deleted'
+            ], 500);
+        }
+    }
+}
